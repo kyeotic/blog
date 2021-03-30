dd4b442b04d20cf3e378e19a12149a892206dbc2
Tim Kye <tim@kye.dev>
2021-03-30 14:13:03 -0700

    add deploy restart to publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,9 +2,10 @@
 
 _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pushd "${_dir}/.."
+pushd "${_dir}/../blog"
 
 docker build -t gcr.io/tk8-cluster/blog .
 docker push gcr.io/tk8-cluster/blog
+kubectl rollout restart deployment blog -n blog
 
 popd
